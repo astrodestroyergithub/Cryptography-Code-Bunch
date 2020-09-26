963d7efa244ed3f0482ee5eed19aa3ae12df4147
Tamojit Roy <67942951+astrodestroyergithub@users.noreply.github.com>
2020-09-26 21:10:17 +0530

    Added Atbash-Cipher/Java Code

---
+++ AtbashCipher.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class MyProgram {
+	
+	static String encrypt(String pt) {
+		int a,i;
+		char ch;
+		char[] encpt1 = pt.toCharArray();
+		for(i=0;i<pt.length();i++) {
+			a=encpt1[i];
+			a=91-(a-64);
+			ch=(char) a;
+			encpt1[i]=ch;
+		}
+		String encpt2 = new String(encpt1);
+		return encpt2;
+	}
+	
+	static String decrypt(String encpt) {
+		int a,i;
+		char ch;
+		char[] decpt1 = encpt.toCharArray();
+		for(i=0;i<encpt.length();i++) {
+			a=decpt1[i];
+			a=91-a+64;
+			ch=(char) a;
+			decpt1[i]=ch;
+		}
+		String decpt2 = new String(decpt1);
+		return decpt2;
+	}
+	
+	public static void main(String[] args) {
+		String pt,encpt,decpt;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter plaintext :");
+		pt=scanner.nextLine();
+		
+		encpt=encrypt(pt);
+		System.out.println("Encrypted text :");
+		System.out.println(encpt);
+		
+		decpt=decrypt(encpt);
+		System.out.println("Decrypted text :");
+		System.out.println(decpt);
+	}
+
+}
+
