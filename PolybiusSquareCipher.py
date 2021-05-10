67c8e0e885f17a8fdee40064de8153c6c0681629
Tamojit Roy <67942951+astrodestroyergithub@users.noreply.github.com>
2021-05-11 01:04:35 +0530

    Formatted PolybiusSquareCipher/Python Code

--- PolybiusSquareCipher.py
+++ PolybiusSquareCipher.py
@@ -1,98 +1,39 @@
-#### Encryption is happening here 
------
+#### Encryption is happening here -----
 def polybiusEncrypt(s):
-encpt=
-""
-for
-char
-in s:
-row = 
-int
-((ord(
-char
-) 
--
-ord(
-'a'
-)) / 5) + 1
-col = ((ord(
-char
-) 
--
-ord(
-'a'
-)) % 5) + 1
-if
-char
-== 
-'k'
-:
-row = row 
--
-1
-col = 5 
--
-col + 1
-elif ord(
-char
-) >= ord(
-'j'
-):
-if
-col == 1 :
-col = 6
-row = row 
--
-1
-col = col 
--
-1
-r=str(row)
-c=str(col)
-encpt = encpt+r+c
-return
-encpt
+encpt=""
+for char in s:
+  row = int((ord(char)-ord('a')) / 5) + 1
+  col = ((ord(char)-ord('a')) % 5) + 1
+  if char== 'k':
+    row = row-1
+    col = 5-col + 1
+  elif ord(char) >= ord('j'):
+    if col == 1 :
+      col = 6
+      row = row-1
+    col = col-1
+  r=str(row)
+  c=str(col)
+  encpt = encpt+r+c
+ return encpt
 
-#### 
-Decryption is happening here 
------
+#### Decryption is happening here -----
 def polybiusDecrypt(s):
 s1 = list(s)
-decpt = 
-""
-for
-i in range(0,len(s),2):
-r = 
-int
-(s1[i])
-c = 
-int
-(s1[i+1])
-ch = chr(((r
--
-1)*5+c+96))
-if
-(ord(ch)
--
-96>=10): 
-ch = chr(((r
--
-1)*5+c+96+1))
-ch1 = 
-str(ch)
-decpt = decpt + ch1
-return
-decpt
-#### Driver code 
------
-pt = input(
-"Enter plaintext: "
-)
+decpt = ""
+for i in range(0,len(s),2):
+  r = int(s1[i])
+  c = int(s1[i+1])
+  ch = chr(((r-1)*5+c+96))
+  if (ord(ch)-96>=10): 
+    ch = chr(((r-1)*5+c+96+1))
+  ch1 = str(ch)
+  decpt = decpt + ch1
+ return decpt
+
+#### Driver code -----
+pt = input("Enter plaintext: ")
 encpt = polybiusEncrypt(pt)
-print(
-"Encrypted text: "
-,encpt)
+print("Encrypted text: ",encpt)
 decpt = polybiusDecrypt(encpt)
-print(
-"Decrypted text: "
-,decpt)
+print("Decrypted text: ",decpt)
